ee66f8f8b6ef482fccf5a38027c2a6e50c498757
ya'akov <diogobonofre@gmail.com>
2024-11-19 21:25:44 -0300

    Feature: implemented side navigation menu + Basic responsivity

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-function toggleMenuButton(button) {
-  button.classList.toggle("change");
-}
-
 window.addEventListener("load", () => {
   const banner = document.querySelector(".banner");
 
-  // Exibe o banner com atraso para garantir que o header já está carregado
   if (banner) {
     setTimeout(() => {
       banner.classList.add("fade-in");
-    }, 200); // Atraso de 500ms (ajuste conforme necessário)
+    }, 200);
   }
 });
+
+const menu = document.querySelector(".menu-navigation");
+
+scrollableDiv.addEventListener("wheel", (e) => {
+  e.stopPropagation();
+});
+
+function toggleMenuNavigation(button) {
+  toggleMenuButton(button);
+
+  if (menu.style.visibility === "visible") {
+    menu.style.visibility = "hidden";
+    menu.style.transform = "translate(-100%)";
+  } else {
+    menu.style.visibility = "visible";
+    menu.style.transform = "translate(0)";
+  }
+}
+
+function toggleMenuButton(button) {
+  button.classList.toggle("change");
+}
